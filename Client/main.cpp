3e94f14fb1090a6f917443fc29274a5afb46a64c
Sergey Zagrebin <zagrebin.dev@gmail.com>
2019-02-24 12:09:54 +0300

    qregister

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,8 +1,12 @@
 #include <QApplication>
 #include "client.h"
 
+Q_DECLARE_METATYPE(std::string)
+
 int main(int argc, char *argv[])
 {
+    qRegisterMetaType<std::string>();
+
     QApplication app(argc, argv);
     QApplication::setApplicationDisplayName(Client::tr("Game Client"));
     Client client;
